40949fa9b4943d44e0adb7c8bf0df5ddbefbafdd
Tommy Xu <tommysteryy@gmail.com>
2024-01-02 12:05:09 +0800

    added hyperlink example

--- flash/test_multipage.py
+++ flash/test_multipage.py
@@ -20,7 +20,10 @@ page1_md="""# flash
 ## Click here to see your results!
 <|button|on_action=change_content|>
 
-<|part|render={result_ready}|page=result|height=100vh|>
+
+<|part|render={result_ready}|class_name=card|
+Click [**here**](/result) for your result!
+|>
 
 This application was created with [Taipy](https://www.taipy.io/).
 
